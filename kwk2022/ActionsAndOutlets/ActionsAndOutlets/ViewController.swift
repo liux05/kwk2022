a0ac416ea3928fba1db0a76e9e6d14e06e2c3053
celine <89039323+liux05@users.noreply.github.com>
2022-08-10 11:57:50 -0400

    initial commit

---
+++ kwk2022/ActionsAndOutlets/ActionsAndOutlets/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ActionsAndOutlets
+//
+//  Created by scholar on 8/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func buttonTap(_ sender: UIButton) {
+        if let newTitle = textField.text {
+            titleLabel.text = newTitle
+        }
+    }
+    
+}
